482e34bd291b34525f3fd180f6ee793733991473
apple <apple@apples-MacBook-Air.local>
2026-03-27 18:18:20 +0530

    Fix catch-all route for Express 5

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('/(.*)', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
